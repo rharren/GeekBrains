ea8c7fbb76dff5d2c1f668fae6cf1cc9243fe5c4
Salavat Ilyasov <ilyasov.s.r@gmail.com>
2023-04-16 14:34:35 +0300

    hw_1 added

--- Pyhton_course/Lesson_1/hw_1.py
+++ Pyhton_course/Lesson_1/hw_1.py
@@ -1,7 +1,9 @@
+#Найдите сумму цифр трехзначного числа.
 print("Enter 3 digit number:")
 number = str(input())
-#for i in num:
-#   if 
-#    a = int(num[0])
-#
-print(len(number))
+
+if len(number) != 3:
+    print("Wrong umber entered. You have to enter 3 digit number!")
+else:
+    sum = int(number[0]) + int(number[1]) + int(number[2])
+    print("Sum of number's digits is {}".format(sum))
